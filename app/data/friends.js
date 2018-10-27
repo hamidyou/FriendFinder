0aea618edadba7e7a9e19dcd64abf9101abd3a26
Youssef <yhamid1982@gmail.com>
2018-10-26 20:18:06 -0400

    logic working to get most compatible friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,39 @@
-const friends = [
+const express = require('express')
+
+// Create router variable
+const router = express.Router()
+
+let friends = [
   {
     name: 'Ahmed',
     photo: '',
-    scores: [5, 2]
+    scores: [1, 2, 3, 4, 5, 6, 1, 2, 3, 4]
   },
   {
     name: 'Youssef',
     photo: '',
-    scores: [5, 2]
+    scores: [5, 6, 1, 2, 3, 4, 5, 6, 1, 2]
+  },
+  {
+    name: 'Becky',
+    photo: '',
+    scores: [3, 4, 5, 6, 5, 4, 3, 2, 1, 2]
+  },
+  {
+    name: 'Ameer',
+    photo: '',
+    scores: [3, 4, 5, 6, 1, 2, 3, 4, 5, 6]
+  },
+  {
+    name: 'Zayne',
+    photo: '',
+    scores: [6, 5, 4, 3, 2, 1, 5, 4, 3, 2]
   }
 ]
+
+// Displays all friends in JSON format
+router.get('/api/friends', function (req, res) {
+  return res.json(friends)
+})
 
 module.exports = friends
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require('express')
 const friends = require('../data/friends')
+const { concat, pipe, sub, reduce, min, findIndex, map, curry, compose, max } = require('kyanite/dist/kyanite')
 
 // Create router variable
 const router = express.Router()
@@ -10,13 +11,31 @@ router.get('/api/friends', function (req, res) {
   return res.json(friends)
 })
 
+const reducer = x => reduce((acc, num) => Number(acc) + Number(num), 0, x)
+
+const mapper = curry((x, y) => map(a => Math.abs(y - reducer(a.scores)), x))
+
+const getIndex = curry((arr, num) => findIndex(x => x === num, arr))
+
+const getName = curry((arr, idx) => friends[idx].name)
+
+const differenceArray = (x, arr) => compose(mapper(arr), reducer, x)
+
+//   pipe([
+//   reducer, // sum of scores
+//   mapper(arr), // [difference of scores compared to all friends]
+//   min // return the lowest difference
+// ], x)
+
 // Create New Characters - takes in JSON input
 router.post('/api/friends', function (req, res) {
-  console.log(req)
   const newFriend = req.body
-  newFriend.routeName = newFriend.name.replace(/\s+/g, '').toLowerCase()
 
-  console.log(newFriend)
+  const diffArray = differenceArray(newFriend.scores, friends)
+  const low = min(diffArray)
+  const findMatch = (num, arr) => compose(getName(arr), getIndex(arr), num)
+
+  console.log(findMatch(low, diffArray))
 
   res.json(newFriend)
 })
