5fc0b2ede9fa5e5e1e345dde2351935b58d52d65
Youssef <yhamid1982@gmail.com>
2018-10-30 18:17:51 -0400

    added background

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,17 @@ const fs = require('fs')
 // Create router variable
 const router = express.Router()
 
+// const getFriends = () => {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile('app/data/friends.json', (err, data) => {
+//       if (err) {
+//         return reject(err)
+//       }
+//       resolve(data)
+//     })
+//   })
+// }
+
 // Displays all friends in JSON format
 router.get('/api/friends', function (req, res) {
   return res.json(friends)
@@ -14,20 +25,20 @@ router.get('/api/friends', function (req, res) {
 const reducer = x => reduce((acc, num) => Number(acc) + Number(num), 0, x)
 const mapper = curry((x, y) => map(a => Math.abs(y - reducer(a.scores)), x))
 const getIndex = curry((arr, num) => findIndex(x => x === num, arr))
-const getInfo = curry((arr, idx) => [friends[idx].name, friends[idx].photo])
 const differenceArray = (x, arr) => compose(mapper(arr), reducer, x)
+const getInfo = curry((arr, idx) => [friends[idx].name, friends[idx].photo])
 const findMatch = (num, arr) => compose(getInfo(arr), getIndex(arr), num)
 
-// const addFriend = y => {
-//   const path = 'app/data/friends.json'
+const addFriend = y => {
+  const path = 'app/data/friends.json'
 
-//   fs.writeFile(path, JSON.stringify(y), (err) => {
-//     if (err) {
-//       throw err
-//     }
-//     console.log('File updated successfully.')
-//   })
-// }
+  fs.writeFile(path, JSON.stringify(y), (err) => {
+    if (err) {
+      throw err
+    }
+    console.log('File updated successfully.')
+  })
+}
 
 // const match = x => {
 //   fs.writeFile('app/data/results.json', JSON.stringify(x), err => {
@@ -48,7 +59,7 @@ router.post('/api/friends', function (req, res) {
 
   friends = concat(newFriend, friends)
 
-  // addFriend(friends)
+  addFriend(friends)
   // match(newFriend)
 
   console.log('Results updated successfully.')
